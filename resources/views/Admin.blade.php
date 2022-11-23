cff01184c48ac2f35cf6186060d8ba3db2c2a358
mohammedboreny <Mohammed.boreny@gmail.com>
2022-11-23 17:55:37 +0300

    SignIn and Admin Page Created

---
+++ resources/views/Admin.blade.php
@@ -0,0 +1,35 @@
+@extends('Master')
+
+@section('content')
+ 
+
+<div class="jumbotron jumbotron-fluid bg-primary">
+
+    <div class="container">
+
+        <h1 class="display-3 text-center">Patient Edit </h1>
+        <p class="lead">
+        </p>
+    </div>
+</div>
+
+<div class="card">
+    <div class="card-header">
+      Featured
+    </div>
+    <div class="card-body">
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <div class="card">
+        <div class="card-header">
+          Featured
+        </div>
+        
+        
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+    
+@endsection
